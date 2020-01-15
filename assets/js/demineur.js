62d5268d8c08e906b9283e8636044fd50c24d998
Furradev <brian.lecarpentier@edu.itescia.fr>
2020-01-15 11:33:18 +0100

    initialise start function

--- assets/js/demineur.js
+++ assets/js/demineur.js
@@ -8,36 +8,49 @@ var end = 0;
 var diff = 0;
 var stoped = false;
 
-function chrono(){
-    startB.onclick = function started(){
-        startB.disabled = true;
-        if(!stoped)
-            start = new Date();
-        else{
-            start = new Date() - diff;
-            start = new Date(start);
-        }
-        refresh();
-    };
-
-    function refresh(){
-        end = new Date();
-        diff = end - start;
-        diff = new Date(diff);
-
-        var sec = diff.getSeconds();
-        var min = diff.getMinutes();
-
-        if (min < 10){
-            min = "0" + min;
-        }
-        if (sec < 10){
-            sec = "0" + sec;
-        }
-        console.log("test");
-        timer.text(min + ":" + sec);
-        timerID = setTimeout(refresh, 50);
+function chrono() {
+    startB.attr('disabled', true);
+    if (!stoped)
+        start = new Date();
+    else {
+        start = new Date() - diff;
+        start = new Date(start);
     }
+    refresh();
 }
 
-chrono();
+function refresh() {
+    end = new Date();
+    diff = end - start;
+    diff = new Date(diff);
+
+    var sec = diff.getSeconds();
+    var min = diff.getMinutes();
+
+    if (min < 10) {
+        min = "0" + min;
+    }
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    console.log("test");
+    timer.text(min + ":" + sec);
+    timerID = setTimeout(refresh, 50);
+}
+
+function init() {
+
+}
+
+function startGame() {
+    generateTable();
+    chrono();
+}
+
+function generateTable() {
+
+}
+
+function reset() {
+
+}
